55fca7a6c77cbbe077e1af7c1377aede82f2b81a
Chris Jordan <cjorda15@msudenver.edu>
2017-06-15 14:09:58 -0600

    further attempt with heroku

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.get('/', (req,res) => {
   res.sendFile(path.resolve(__dirname+"/../src/index.html"))
 })
 
-app.get('/*', function (req, res) { path.resolve(__dirname+"/../src/index.html") });
+app.get('/*', function (req, res) { path.resolve(__dirname, "src/index.html") });
 
 
 
